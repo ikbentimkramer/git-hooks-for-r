8d584734cd5e9efa7c17aa3ec2e4888f6008fe2b
ikbentimkramer <53525709+ikbentimkramer@users.noreply.github.com>
2019-08-08 13:52:14 +0200

    Initial commit

---
+++ pre-commit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+######################################################################
+## filename: pre-commit.sh
+## description: R-focussed pre-commit hook for GIT
+## arguments: 
+## author: Tim Kramer
+######################################################################
+
+######################################################################
+# This scripts assumes the following:
+#  - R is installed
+#  - Rscript is in $PATH
+#  - The R package 'devtools' is installed
+######################################################################
+
+exit
+
