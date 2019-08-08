2df360ef062744003816d9b0a15a57cdbc483937
ikbentimkramer <53525709+ikbentimkramer@users.noreply.github.com>
2019-08-08 18:55:01 +0200

    Add auto-stash to pre-commit
    Unstaged files in the working directory can infuence test results when
    testing a commit. This is unwanted behaviour, because we want to test
    the commit only. To solve this, we can store the unstaged files in a
    stash, so they are no longer in the working directory when tests are
    run. That is exactly what this commit does: it stashes the working
    directory in a timestamped stash. It unstashes after tests are run.

--- pre-commit.sh
+++ pre-commit.sh
@@ -14,9 +14,28 @@
 #  - The R package 'devtools' is installed
 ######################################################################
 
+# In order to know if a stash has been made, we need to compare the
+# stash stack before and after we store the stash. If both stacks are
+# the same, nothing was stored.
+OLD_STASH=$(git rev-parse -q --verify refs/stash)
+# Stash all unstaged and untracked files
+git stash push -q -u --keep-index
+NEW_STASH=$(git rev-parse -q --verify refs/stash)
+
+# If there were no changes, nothing got stashed and nothing should
+# run.
+if [ "$OLD_STASH" = "$NEW_STASH" ]; then
+    echo "Pre-commit: no changes to test"
+    exit 0
+fi
+
 # Run tests
 exec Rscript -e "library(devtools)" -e "devtools::load_all()" \
      -e "devtools::check()"
 
-exit
+# Add unstaged and untracked files back in
+git reset --hard -q
+git clean -fd -q
+git stash pop --index -q
 
+exit 0
